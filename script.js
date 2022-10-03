8a305060c46909ddd370fce215e5b4b5741283b6
Tinyefuza <gordon.tinyefuza@gmail.com>
2022-10-03 11:55:04 +0300

    Append API posts to the DOM

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const getText = document.getElementById('getText');
 const getJson = document.getElementById('getJson');
+const getPosts = document.getElementById('getPosts');
 
 const addText = () => {
     // Fetch method takes two parameters the request url and returns a promise
@@ -45,5 +46,30 @@ const addJson = () => {
     })
 }
 
+const addPosts = () => {
+    // Fetch method takes two parameters the request url and returns a promise
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        // Append Posts from the API to the DOM or webpage
+        let output = document.getElementById('output');
+        let posts = `<h2>Posts</h2>`;
+        data.forEach((post) => {
+            posts += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            `
+            output.innerHTML = posts;
+        });
+    })
+    // To Catch Errors
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
 getText.addEventListener('click', addText);
 getJson.addEventListener('click', addJson);
+getPosts.addEventListener('click', addPosts);
